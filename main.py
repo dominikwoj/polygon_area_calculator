11dd0b51ac2612938e97c16cbe174b6131a01baa
Dominik Wojnowski <d.wojnowski.priv@gmail.com>
2024-03-12 23:03:45 +0100

    updated Rectangle class

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 class Rectangle:
-    pass
+    width = 0
+    height = 0
+    def __init__(self):
+        pass
 
-class Square:
+    def set_width(self, w):
+        self.width = w
+    def set_height(self, h):
+        self.height = h
+
+    def get_area(self):
+        return self.height * self.height
+
+    def get_perimeter(self):
+        return 2 * self.height * 2 * self.width
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+class Square(Rectangle):
     pass
